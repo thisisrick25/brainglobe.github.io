27e74a58a8d0df8245c375059d5fda13fa5d2131
thisisrick25 <swapnaneel06@gmail.com>
2025-05-08 17:30:45 +0530

    Remove --mirror option from git clone command in fetch_external.py

--- fetch_external.py
+++ fetch_external.py
@@ -17,7 +17,7 @@ REPOS = [
 
 for url, path in REPOS:
     if not os.path.exists(path):
-        subprocess.run(["git", "clone", "--mirror", url, path])
+        subprocess.run(["git", "clone", url, path])
     else:
         subprocess.run(["git", "-C", path, "pull"])
         
